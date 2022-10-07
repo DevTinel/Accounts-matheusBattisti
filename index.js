06059d4159dd1845219259c217f449af250db6b8
DevTinel <davidaugustolol36@gmail.com>
2022-10-07 19:46:48 -0300

    deposito ,adição e consult saldo

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ import chalk from 'chalk';
 import fs from 'fs';
 
 console.log(chalk.bgBlue.white.bold('iniciamos o account'));
-operator();
+operation();
+//exibindo as acoes para usuario
 
-function operator() {
+function operation() {
   inquirer
     .prompt([
       {
         type: 'list',
         name: 'action',
         message: 'O que você deseja fazer?',
-        choices: ['Criar conta', 'Consultar saldo', 'Depositar', 'Sair'],
+        choices: [
+          'Criar conta',
+          'Consultar saldo',
+          'Depositar',
+          'Sacar',
+          'Sair',
+        ],
       },
     ])
     .then((answer) => {
@@ -25,8 +32,13 @@ function operator() {
           createAccount();
           break;
         case 'Consultar saldo':
+          getAccountBalance();
           break;
         case 'Depositar':
+          deposit();
+          break;
+        case 'Sacar':
+          widthDraw();
           break;
         case 'Sair':
           Exit();
@@ -42,13 +54,12 @@ function operator() {
       console.log(err);
     });
 }
-
+//criando a conta
 function createAccount() {
   console.log(chalk.bgGreen.bold('Parabens por escolher o nosso banco!'));
   console.log(chalk.bgGreen.bold('Defina as opções da sua conta a seguir.'));
   buildAccount();
 }
-
 function buildAccount() {
   inquirer
     .prompt([
@@ -70,18 +81,116 @@ function buildAccount() {
         buildAccount();
         return;
       }
-      fs.writeFileSync(`accounts/${name}.json`, '{"ballance":0}', (err) => {
+      fs.writeFileSync(`accounts/${name}.json`, '{"balance":0}', (err) => {
         console.log(err);
       });
       console.log(chalk.green('Sua conta foi criada'));
-      operator();
+      operation();
     })
     .catch((erro) => {
       console.log(err);
     });
 }
 
+//funcao para sair
 function Exit() {
   console.log(chalk.bgBlue.black('Obrigado por usar o Accounts '));
   process.exit();
+}
+
+//função para deposito
+function deposit() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Qual nome da sua conta',
+      },
+    ])
+    .then((resposta) => {
+      const accountName = resposta['accountName'];
+      //verificar se a conta existe
+      if (!checkAccount(accountName)) {
+        return deposit();
+      }
+      inquirer
+        .prompt([
+          {
+            name: 'amount',
+            message: 'Quanto você deseja depositar?',
+          },
+        ])
+        .then((answer) => {
+          const amount = answer['amount'];
+          addAmount(accountName, amount);
+          operation();
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((erro) => console.log(err));
+}
+
+function checkAccount(accountName) {
+  if (!fs.existsSync(`accounts/${accountName}.json`)) {
+    console.log(chalk.bgRed('Esta conta não existe, escolha outro nome!'));
+    return false;
+  }
+  return true;
+}
+
+function addAmount(accountName, amount) {
+  const account = getAccount(accountName);
+  if (!amount) {
+    console.log(
+      chalk.bgRed.black('Ocorreu um erro ,tente novamente mais tarde'),
+    );
+    return deposit();
+  }
+  account.balance = parseFloat(amount) + parseFloat(account.balance);
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(account),
+    (err) => {
+      console.log(chalk.bgRed(err));
+    },
+  );
+  console.log(
+    chalk.green(`Foi depositado o valor de R$ ${amount} na sua conta.`),
+  );
+}
+
+function getAccount(accountName) {
+  const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+    encoding: 'utf8',
+    flag: 'r',
+  });
+  return JSON.parse(accountJSON);
+}
+
+function getAccountBalance() {
+  inquirer
+    .prompt([
+      {
+        name: 'account',
+        message: 'Qual o nome da sua conta',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer['account'];
+      if (!checkAccount(accountName)) {
+        return getAccountBalance();
+      }
+      const account = getAccount(accountName);
+      console.log(
+        chalk.bgBlue.black(
+          `Olá , o saldo da sua conta é de R$ ${account.balance}`,
+        ),
+      );
+      operation();
+    })
+    .catch((err) => console.log(err));
+}
+
+function widthDraw() {
+  
 }
